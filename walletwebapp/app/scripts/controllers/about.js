b2bdb6e5831d7e2cb02aaf9e8a953820f2b3bdc8
unknown <matteo.gilardoni@User-PC.sol.kurtosys.com>
2015-01-15 15:25:25 +0000

    scaffolding done and working

---
+++ walletwebapp/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name walletwebappApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the walletwebappApp
+ */
+angular.module('walletwebappApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
